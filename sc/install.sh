da339dac6e4a51943b7ad27defac14bba8f05181
Turtles127 <104092144+Turtles127@users.noreply.github.com>
2024-05-19 20:46:53 -0700

    Update install.sh

--- sc/install.sh
+++ sc/install.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+apt-get update
+apt install openjdk-17-jre-headless -y
+wget https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar
+java -Xms1G -Xmx2G -jar server.jar nogui
+sed -i '3d' eula.txt
+echo "eula=true" >> eula.txt
+java -Xms1G -Xmx8G -jar server.jar nogui
